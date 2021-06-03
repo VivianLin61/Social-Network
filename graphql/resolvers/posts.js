fbd5dc8a7bddae237b462d5dcf88a3585982ec35
Vivian Lin <linvivian61@gmail.com>
2021-06-03 17:27:35 -0400

    like and commnet count

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -3,6 +3,7 @@ const Post = require('../../models/Post')
 const checkAuth = require('../../util/check-auth')
 module.exports = {
   Query: {
+  
     async getPosts() {
       try {
         const posts = await Post.find()
